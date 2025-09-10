5b896d7202a88227204b54ccd287b84f7cb3b0f9
vlatson647-hash <vlatson647@phm.k12.in.us>
2025-09-10 09:28:08 -0400

    Update gradebookManager.py

--- gradebookManager.py
+++ gradebookManager.py
@@ -1,2 +1,18 @@
+gradebook = {"Ryan": "94, 87, 81", "Mary": "81, 76, 89", "Kyle": "19, 23, 11"}
+
+user_student = input(print("What's the name of the student you'd like to add to your gradebook? "))
+print(user_student)
+gradebook
+
+user_grade = input(print("What grades will this student have?"))
+print(user_grade)
+
+gradebook[user_student] = user_grade
+print(gradebook)
+
+
+
+
+
 
 #change
