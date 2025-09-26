3b2bc90817b5d186caebbfb40e11bf0a9489a622
vlatson647-hash <vlatson647@phm.k12.in.us>
2025-09-26 09:22:39 -0400

    Create df_data.py

---
+++ df_data.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as ply
+import random
+
+# load our data frame
+df = pd.read_csv("pennData.csv")
+pennData = pd.DataFrame(df)
+print("-_"*20)
+print("Head of the Dataframe") 
+print(pennData.head())
+
+
+print("-_"*20)
+print("Tail of the Dataframe") 
+print(pennData.tail())
+
+
+print("-_"*20)
+print("Summary of the Dataframe") 
+print(pennData.info())
+
+
+print("-_"*20)
+print("Statistiics") 
+print(round(pennData.describe()))
+
+
+print("-_"*20)
+print("Count of students in Pathways") 
+print(pennData['Pathway'].value_counts())
+
+
+print("-_"*20)
+print("average GPA per Year") 
+print(pennData.groupby('Year')['GPA'].mean())
+
+
+print("-_"*20)
+print("Top 3 Students by GPA") 
+print(pennData.sort_values(by='GPA', ascending=False).head(3))
+
+
+print("-_"*20)
+print("Students with GPA greater than 3.5") 
+print(pennData[pennData['GPA']>3.5])
+
+
+print("-_"*20)
+print("First Student Data") 
+print(pennData.iloc[0] )
